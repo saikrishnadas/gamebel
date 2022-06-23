5968beeb72a956938aec893c1ff7f33d8d668088
0xC137 <91955270+streetU@users.noreply.github.com>
2022-06-23 15:42:04 +0200

    progress

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,5 @@
+import { Progress } from "antd";
+
 function Footer() {
   return (
     <footer className="p-4 bg-white shadow md:px-6 md:py-8 dark:bg-gray-800">
@@ -36,6 +38,7 @@ function Footer() {
           </li>
         </ul>
       </div>
+      <Progress percent={50} status="active" />
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2022{" "}
