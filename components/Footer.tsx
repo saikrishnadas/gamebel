fe4004388cc5c5417ea38ae720c767741e8722fa
0xC137 <91955270+streetU@users.noreply.github.com>
2022-06-23 16:14:18 +0200

    footer double

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,8 +37,30 @@ function Footer() {
             </a>
           </li>
         </ul>
+        <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+          <li>
+            <a href="#" className="mr-4 hover:underline md:mr-6 ">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="mr-4 hover:underline md:mr-6">
+              Privacy Policy
+            </a>
+          </li>
+          <li>
+            <a href="#" className="mr-4 hover:underline md:mr-6 ">
+              Licensing
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline">
+              Contact
+            </a>
+          </li>
+        </ul>
       </div>
-      <Progress percent={50} status="active" />
+      <Progress percent={89} status="active" />
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2022{" "}
