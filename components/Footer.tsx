e486b1d69bb3979e29eb6efa692548d3daf4bad6
0xC137 <91955270+streetU@users.noreply.github.com>
2022-06-23 16:23:31 +0200

    single menu

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import { Progress } from "antd";
 
 function Footer() {
   return (
-    <footer className="p-4 bg-white shadow md:px-6 md:py-8 dark:bg-gray-800">
+    <footer className="p-4 bg-dark shadow md:px-6 md:py-8 dark:bg-gray-800">
       <div className="sm:flex sm:items-center sm:justify-between">
         <span  className="flex items-center mb-4 sm:mb-0">
         {/* <a
@@ -10,33 +10,11 @@ function Footer() {
           className="flex items-center mb-4 sm:mb-0"
         > */}
           {/* <img src="/docs/images/logo.svg" className="mr-3 h-8" alt="Flowbite Logo"> */}
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
             LOGO
           </span>
         {/* </a> */}
         </span>
-        <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
-          <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6">
-              Privacy Policy
-            </a>
-          </li>
-          <li>
-            <a href="#" className="mr-4 hover:underline md:mr-6 ">
-              Licensing
-            </a>
-          </li>
-          <li>
-            <a href="#" className="hover:underline">
-              Contact
-            </a>
-          </li>
-        </ul>
         <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
           <li>
             <a href="#" className="mr-4 hover:underline md:mr-6 ">
